72db9821b3ea48b8cc277e11c1937b5e92cd4247
ayfor <josh.a.stubbington@gmail.com>
2021-07-01 17:20:30 -0400

    added get with id product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,21 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include:[{model: Tag, through:ProductTag, as:'categories'}]
+    });
+
+    if(!productData){
+      res.status(404).json({message:'No product found with this id!'})
+      return;
+    }
+
+
+    res.status(200).json(productData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 // create new product
